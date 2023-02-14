0a8f7260de529090615fcd389bdcd3aa3fa8003d
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-02-14 18:59:47 +0800

    e

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 pub mod cringe;
+pub mod caps;
 
-use clap::{Args, Parser, Subcommand};
 use crate::cringe::cringe::cringeText;
+use crate::caps::caps::{allCaps, allNoCaps};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "txttools")]
@@ -28,7 +30,7 @@ use crate::cringe::cringe::cringeText;
 #[command(about = "Text Tools written in Rust", long_about = "Text tools written in Rust.\nCopyright (C) 2023  Sherlock\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>.")]
 #[command(propagate_version = true)]
 struct Cli {
-    /// Which text mode to use
+    /// Which text command to use.
     #[command(subcommand)]
     mode: Modes,
 
@@ -36,16 +38,30 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Modes {
-    /// Adds files to myapp
+    /// Makes your text look cringe.
     Cringe(Cringe),
+    /// Capitalise all letters.
+    Caps(Caps),
+    /// Un-capitalise all letters aka make them small.
+    Nocaps(NoCaps),
 }
 
 #[derive(Args)]
 struct Cringe {
-    /// retardation factor (or level).  The higher the worse the capitalisation.
+    /// retardation factor (or level).  The higher the worse the capitalisation.  Accepted values: 1 - 9.
     #[arg(short, long, default_value_t = 8)]
     retard: i32,
     /// The input text
+    text: String
+}
+#[derive(Args)]
+struct Caps {
+    /// The input text.
+    text: String
+}
+#[derive(Args)]
+struct NoCaps {
+    /// The input text.
     text: String
 }
 
@@ -54,6 +70,14 @@ fn main() {
     match &cli.mode {
         Modes::Cringe(i) => {
             let return_text = cringeText(i.text.as_str(), i.retard);
+            println!("{}", return_text);
+        },
+        Modes::Caps(i) => {
+            let return_text = allCaps(i.text.as_str());
+            println!("{}", return_text);
+        },
+        Modes::Nocaps(i) => {
+            let return_text = allNoCaps(i.text.as_str());
             println!("{}", return_text);
         },
     }
